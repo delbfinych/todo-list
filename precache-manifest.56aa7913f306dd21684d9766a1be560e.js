c96b0881d518abd24b600a4baf7d7474151727b3
Villione <dminario@gmail.com>
2019-02-15 22:59:06 +0300

    Updates

---
+++ precache-manifest.56aa7913f306dd21684d9766a1be560e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "01e072c58ccb74dc7a09",
+    "url": "/todo-list/static/js/main.01e072c5.chunk.js"
+  },
+  {
+    "revision": "f92807cd0502e2eaa394",
+    "url": "/todo-list/static/js/1.f92807cd.chunk.js"
+  },
+  {
+    "revision": "01e072c58ccb74dc7a09",
+    "url": "/todo-list/static/css/main.6a156d1f.chunk.css"
+  },
+  {
+    "revision": "02dd140bd81f259c2c6a99379ec605cb",
+    "url": "/todo-list/index.html"
+  }
+];
